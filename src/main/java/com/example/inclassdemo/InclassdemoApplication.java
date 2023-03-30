444935e441eb2594aa8415a902e6baa933c84623
Shih Yao Wang <sywang@sykenwang-MBA.local>
2023-03-30 12:29:53 -0500

    initial commit

---
+++ src/main/java/com/example/inclassdemo/InclassdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.inclassdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclassdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InclassdemoApplication.class, args);
+	}
+
+}
